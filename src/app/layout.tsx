d43a0cd7b588b7529d75b73551f139af831c9687
Jonathan Clem <j@jclem.me>
2023-04-24 11:07:11 -0400

    Add Berkeley Mono and Hanken Grotesk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,35 @@
+import {Hanken_Grotesk} from '@next/font/google'
+import localFont from '@next/font/local'
 import {Analytics} from '@vercel/analytics/react'
+import {cva} from 'class-variance-authority'
 import {FC, PropsWithChildren} from 'react'
 import '../styles/globals.css'
+
+const hankenGrotesk = Hanken_Grotesk({
+  subsets: ['latin'],
+  variable: '--font-sans',
+  display: 'swap'
+})
+
+const berkeleyMono = localFont({
+  src: [
+    {
+      path: '../fonts/BerkeleyMono-Regular.woff2',
+      weight: '400',
+      style: 'normal'
+    }
+  ],
+  variable: '--font-mono',
+  display: 'swap'
+})
+
+const bodyClass = cva([berkeleyMono.className, hankenGrotesk.className])()
 
 const RootLayout: FC<PropsWithChildren> = ({children}) => {
   return (
     <html>
       <head></head>
-      <body>
+      <body className={bodyClass}>
         {children}
         <Analytics />
       </body>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,7 +6,12 @@ const tailwindConfig = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-sans)'],
+        mono: ['var(--font-mono)']
+      }
+    }
   },
   plugins: []
 }
