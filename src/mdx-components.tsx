bc0765bbd51730c7b7d52ede3881a0bcc25f8376
Jonathan Clem <j@jclem.me>
2023-04-24 18:52:14 -0400

    Improved code blocks

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -10,14 +10,26 @@ export function useMDXComponents(
       const language = props.className?.replace(/^language-/, '')
       const isLanguageBlock = props.className?.startsWith('language-')
 
-      return (
+      const code = (
         <code {...props} data-language={language}>
           {props.children}
-          {isLanguageBlock && (
-            <CopyButton copy={String(props.children).trim()} />
-          )}
         </code>
       )
+
+      if (isLanguageBlock) {
+        return (
+          <>
+            <div className="flex items-center justify-end gap-2 border-b border-dashed border-border p-1 text-xs text-text-deemphasize">
+              {language}
+              <CopyButton copy={String(props.children).trim()} />
+            </div>
+
+            <div className="overflow-x-auto p-2 text-xs">{code}</div>
+          </>
+        )
+      } else {
+        return <>{code}</>
+      }
     }
   }
 }
